1c539cce5149c108caf2a7ccb1c7344bb33279c5
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-07-17 13:53:42 +0300

    i edit the welcome message

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const {Telegraf} = require("telegraf")
+// const { StartMessage } = require("./messageHandler");
+const { startMessage } = require("./messageHandler");
+const { Telegraf } = require("telegraf");
 
 const config = require("config");
 
+const bot = new Telegraf(config.get("botToken"));
 
-const bot = new Telegraf(config.get("botToken"))
+bot.start((ctx) => ctx.reply(startMessage()));
 
-bot.start(ctx=>ctx.reply("خوش آمدید به ربات ما"))
-
-bot.launch()
+bot.launch();
---
+++ messageHandler.js
@@ -0,0 +1,11 @@
+module.exports.startMessage = ()=>{
+    return `سلام
+    من ربات سهامجو هستم
+    اسم هر سهامی که دلت خواست رو هر زمانی می تونی برام بفرستی و من اطلاعاتشو بهت برگردونم.
+    لیست قابلیت های من
+    /symbol_list
+    /best_symbols
+    /comp_symbol
+    
+    @stock110bot`
+}
